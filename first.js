13a5dfd8edc5c7820b4c7c9db1ac0b8f863a26f8
rahulbhongade358 <rahulbhongade912@gmail.com>
2025-01-27 22:28:26 +0530

    improved

--- first.js
+++ first.js
@@ -3,24 +3,35 @@ const display = document.getElementById('userinfo');
 const arr = []
 form.addEventListener('submit',function(e){
     e.preventDefault()
-    let name = document.getElementById('name').value
-    let age = parseInt(document.getElementById('age').value)
+    let Name = document.getElementById('name').value
+    let Age = document.getElementById('age').value
     let address = document.getElementById('Address').value
     let userobj = {
-        'Name' : name ,
-        'Age' : age ,
+        'Name' : Name ,
+        'Age' : Age ,
         'Address' : address,
     };
-
+    display.innerHTML=null
     arr.push(userobj)
    for (let i = 0; i < arr.length; i++) {
-    display.innerText = `
-    User ${i+1}:
-    Name: ${arr[i].Name}
-    Age: ${arr[i].Age}
-    Address: ${arr[i].Address}
-    --------------------
-    `;
+    let card = document.createElement("div")
+    card.setAttribute("class", "card");
+    let name= document.createElement("p")
+    name.setAttribute("class" , "name")
+    let age= document.createElement("p")
+    age.setAttribute("class" , "age")
+    let add= document.createElement("p")
+    add.setAttribute("class" , "address")
+    name.innerText= `Name: ${arr[i].Name}`
+    age.innerText= `Age: ${arr[i].Age}`
+    add.innerText=`Address: ${arr[i].Address }`
+
+    card.append(name,age,add)
     
+    display.append(card)
+
    }
+
+   console.log(arr)
+
 })
